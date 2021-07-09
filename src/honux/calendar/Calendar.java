9315eb70956449cea1ef27224e1e9978b332acd6
Hwangwonuk <hok0173@naver.com>
2021-07-09 22:44:44 +0900

    Final

--- src/honux/calendar/Calendar.java
+++ src/honux/calendar/Calendar.java
@@ -1,30 +1,66 @@
 package honux.calendar;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 
 public class Calendar {
 	private static final int[] MAX_DAYS = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 	private static final int[] LEAP_MAX_DAYS = { 0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-
-	private HashMap <Date, String> planMap;
+	private static final String SAVE_FILE="calendar.dat";
+	private HashMap <Date, PlanItem> planMap;
 	
 	public Calendar() {
-		planMap = new HashMap<Date, String>();
-	}
-	
-	public void registerPlan(String strDate, String plan) throws ParseException {
+		planMap = new HashMap<Date, PlanItem>();
+		File f = new File(SAVE_FILE);
+		if (!f.exists()) {
+			System.err.println("no save file");
+			return;
+		}
+		try {
+			Scanner s = new Scanner(f);
+			while(s.hasNext()) {
+				String line = s.nextLine();
+				String[] words = line.split(",");
+				String date = words[0];
+				String detail = words[1].replaceAll("\"", "" );
+				System.out.println(date + ":" + detail);
+				PlanItem p = new PlanItem(date, detail);
+				planMap.put(p.getDate(), p);
+			}
+			s.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 		
-		Date date = new SimpleDateFormat("yyyy-MM-dd").parse(strDate);
-//		System.out.println(date);
-		planMap.put(date, plan);
 	}
 	
-	public String searchPlan(String strDate) throws ParseException {
-		Date date = new SimpleDateFormat("yyyy-MM-dd").parse(strDate);
-		String plan = planMap.get(date);
-		return plan;
+	public void registerPlan(String strDate, String plan) {
+		
+		PlanItem p = new PlanItem(strDate, plan);
+		planMap.put(p.getDate(), p);
+		
+		File f = new File(SAVE_FILE);
+		String item = p.saveString();
+		try {
+			FileWriter fw = new FileWriter(f, true);
+			fw.write(item);
+			fw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+		
+	}
+	
+	public PlanItem searchPlan(String strDate) {
+		Date date = PlanItem.getDatefromString(strDate);
+		return planMap.get(date);
 	}
 	
 	public boolean isLeapYear(int year) {
--- src/honux/calendar/Prompt.java
+++ src/honux/calendar/Prompt.java
@@ -95,15 +95,13 @@ public class Prompt {
 		System.out.println("[일정 검색]");
 		System.out.println("[날짜를 입력해 주세요(yyyy-MM-dd]");
 		String date = s.next();
-		String plan = "";
-		try {
-			plan = c.searchPlan(date);
-		} catch (ParseException e) {
-			e.printStackTrace();
-			System.err.println("일정 검색 중 오류가 발생했습니다.");
+		PlanItem plan;
+		plan = c.searchPlan(date);
+		if (plan != null) {
+			System.out.println(plan.detail);
+		} else {
+			System.out.println("일정이 없습니다.");
 		}
-		System.out.println(plan);
-
 	}
 
 	private void cmdRegister(Scanner s, Calendar c) throws ParseException {
@@ -111,14 +109,13 @@ public class Prompt {
 		System.out.println("[날짜를 입력해 주세요(yyyy-MM-dd]");
 		String date = s.next();
 		String text = "";
-		System.out.println("일정을 입력해 주세요. (문장의 끝에;을 입력해 주세요.)");
-		while (true) {
-			String word = s.next();
+		System.out.println("일정을 입력해 주세요.(끝문자=;)");
+		String word;
+		while(!(word = s.next()).endsWith(";")) {
 			text += word + " ";
-			if (word.endsWith(";")) {
-				break;
-			}
 		}
+		word = word.replace(";", "");
+		text += word;
 		c.registerPlan(date, text);
 	}
 
